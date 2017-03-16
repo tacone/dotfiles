919835dc3ed9343811d3a910a55ebeb492158c6b
tacone <tacone@users.noreply.github.com>
2017-03-16 16:38:20 +0100

    inotify tools in the setup

--- .setup/setup.sh
+++ .setup/setup.sh
@@ -7,6 +7,10 @@ cd ~
 sudo apt-get install git zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# --- filewatch
+
+sudo apt-get install inotify-tools
+
 # set up symlinks
 
 sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
